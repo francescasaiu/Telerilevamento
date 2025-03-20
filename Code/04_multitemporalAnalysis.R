b1d54a6200e0f69b401130ba91bc13c15baaa329
Francesca Saiu <f.saiu@hotmail.it>
2025-03-20 14:23:15 +0100

    Update 04_multitemporalAnalysis.R

--- Code/04_multitemporalAnalysis.R
+++ Code/04_multitemporalAnalysis.R
@@ -28,24 +28,3 @@ plot(gr[[1]], col=rocket(100))
 plot(gr[[4]], col=rocket(100))
 grdiff=gr[[1]]-gr[[4]]
 plot(grdiff)
-
-# Exporting data
-setwd("C:/Users/fsaiu/UNI/MAGISTRALE/TELERILEVAMENTO")
-getwd()
-# png
-png("greenland_output.png")
-plot(gr)
-dev.off()
-# pdf
-pdf("greenland_output.pdf")
-plot(gr)
-dev.off()
-# Con le differenze
-pdf("greenland_differences.pdf")
-plot(grdiff)
-dev.off()
-# jpeg
-jpeg("greenland_differences.jpeg")
-plot(grdiff)
-dev.off()
-
