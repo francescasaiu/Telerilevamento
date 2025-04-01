95d348598d20c1e4d238f246871d93b289c1e8e6
Francesca Saiu <f.saiu@hotmail.it>
2025-04-01 17:32:29 +0200

    Update 07_classification.R

--- Code/07_classification.R
+++ Code/07_classification.R
@@ -1,7 +1,10 @@
-# RF code for classifying images
+# R code for classifying images
+# install.packages("patchwork")
 
 library(terra)
 library(imageRy)
+library(ggplot2)
+library(patchwork)
 
 mato92=im.import("matogrosso_l5_1992219_lrg.jpg")
 mato92=flip(mato92)
@@ -26,3 +29,27 @@ perc92=prop92*100
 tot06=ncell(mato06c)
 perc06=freq(mato06c)*100/tot06
 # antropic = 54%, forest = 45%
+
+# creating a dataframe
+class = c("Forest","Antropic")
+y1992=c(83,17)
+y2006=c(45,55)
+tab=data.frame(class, y1992,y2006)
+
+p1=ggplot(tab, aes(x=class, y=y1992, color=class)) + 
+  geom_bar(stat="identity", fill="white") +
+ylim(c(0,100))
+
+p2=ggplot(tab, aes(x=class, y=y2006, color=class)) + 
+  geom_bar(stat="identity", fill="white") +
+ylim(c(0,100))
+# uso patchwork per mettere i due grafici vicini
+p1+p2
+# per averli uno sopra l'altro
+p1/p2
+
+p0= im.ggplot(mato92)
+p00= im.ggplot(mato06)
+p0+p00+p1+p2
+
+
