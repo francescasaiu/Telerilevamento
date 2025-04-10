f222dc3d1bda75f4bb5dc9a890e999bc12bbfd5a
Francesca Saiu <f.saiu@hotmail.it>
2025-04-10 15:51:46 +0200

    Create 09_packages_gitHub.R

---
+++ Code/09_packages_gitHub.R
@@ -0,0 +1,12 @@
+# R code for installing packages from GitHub
+
+library(devtools) # or remotes
+install_github("ducciorocchini/cblindplot")
+library(cblindplot)
+install_github("clauswilke/colorblindr")
+library(colorblindr)
+
+
+# From CRAN:
+install.packages("colorblindcheck")
+library(colorblindcheck)
