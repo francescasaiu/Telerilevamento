9c68094465e2093bb80382b52287763710fa110f
Francesca Saiu <f.saiu@hotmail.it>
2025-06-22 16:40:38 +0200

    Update exam_code.R

--- Code/Exam/exam_code.R
+++ Code/Exam/exam_code.R
@@ -1,7 +1,8 @@
 # Code part for the exam: Analysis of Etna's plume and sulfur dioxide in atmosphere
 
 # Packages
-library(terra)   #
-library(imageRy) #
-library(viridis) # 
+library(terra)   # for spatial data analysis
+library(imageRy) # to analyze raster images with R
+library(viridis) # useful for changing color ramp palette
 
+# Setting the working directory
