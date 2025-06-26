918355a8bfdb3f5d816bb4673dd83d315c0ee0dc
Francesca Saiu <f.saiu@hotmail.it>
2025-06-26 16:22:39 +0200

    Update exam_code.R

--- Code/Exam/exam_code.R
+++ Code/Exam/exam_code.R
@@ -24,38 +24,30 @@ nov7 = flot(nov7, "7-11.jpg")
 
 nov12 = flot(nov12, "12-11.jpg")
 
-nov29 = flot(nov29, "29-11.jpg")
-
 dec30 = flot(dec30, "30-12.jpg")
 
-so2 = flot(so2, "SO2.jpg")
+so2_02 = flot(so2_02, "SO2.jpg")
 
+so2_24 = flot(so2_24, "SO2-2024.jpg")
+
+so2_25 = flot(so2_25, "SO2-2025.jpg")
 
 
 # Plotting the images chronologically to show the evolution of the eruptive plume
-im.multiframe(3,3)
-plot(oc27, main="October 27")
-plot(oc29, main="October 29")
-plot(oc30, main="October 30")
-plot(oc31, main="October 31")
-plot(nov4, main="November 4")
-plot(nov7, main="November 7")
-plot(nov12, main="November 12")
-plot(nov29, main="November 29")
-plot(dec30, main="December 30")
+im.multiframe(2,3)
+plot(oc27)
+plot(oc29)
+plot(oc30)
+plot(nov7)
+plot(nov12)
+plot(dec30)
 # we can see that the eruptive column tends to be transported by the wind and seems to follow a circular path, on November 29th it begins to wane and on December 30th its practically gone
 
-# non mi piace, forse devo fare la sottrazione, domani ci provo
+# 
 im.multiframe(3,4)
-plot(oc27[[1]], col=viridis(100), main="October 27")
-plot(oc29[[1]], col=viridis(100))
-plot(oc30[[1]], col=viridis(100))
-plot(oc31[[1]], col=viridis(100))
-plot(nov4[[1]], col=viridis(100))
-plot(nov7[[1]], col=viridis(100))
-plot(nov12[[1]], col=viridis(100))
-plot(nov29[[1]], col=viridis(100))
-plot(dec30[[1]], col=viridis(100))
+plot(oc27[[1]], col=cividis(100))
+colindex = oc27[[1]]-oc27[[3]]
+plot(colindex)
 
 
 
